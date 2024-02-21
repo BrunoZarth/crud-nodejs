d3f9d13c5ba66785e5d520a92870cbde19862912
Bruno Zarth <brunozarth@gmail.com>
2024-02-21 01:17:59 -0300

    Refact: running version 1.0

--- init.sql
+++ init.sql
@@ -18,20 +18,20 @@ CREATE TABLE product (
 CREATE TABLE request (
     id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
     quantity INTEGER NOT NULL,
-    product_id UUID DEFAULT uuid_generate_v4 () NOT NULL,
+    product_id UUID NOT NULL,
     FOREIGN KEY (product_id) REFERENCES product (id)
 );
 
 CREATE TABLE cart (
     id UUID DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    users_id UUID DEFAULT uuid_generate_v4 () NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    users_id UUID NOT NULL,
+    created_at TIMESTAMP,
     FOREIGN KEY (users_id) REFERENCES users (id)
 );
 
 CREATE TABLE cart_request (
-    cart_id UUID DEFAULT uuid_generate_v4 () NOT NULL,
-    request_id UUID DEFAULT uuid_generate_v4 () NOT NULL,
+    cart_id UUID NOT NULL,
+    request_id UUID NOT NULL,
     FOREIGN KEY (cart_id) REFERENCES cart (id),
     FOREIGN KEY (request_id) REFERENCES request (id)
 );
--- src/routes/cartRequestRoutes.js
+++ src/routes/cartRequestRoutes.js
@@ -5,13 +5,9 @@ const { tokenValidated } = require("../middleware/auth");
 
 router.get("/", cartRequestController.getAllCartRequest);
 
-router.get("/:id", cartRequestController.getCartRequestById);
-
 router.post("/", cartRequestController.createCartRequest);
 
 router.put("/:id", tokenValidated, cartRequestController.updateCartRequest);
-
-router.delete("/:id", tokenValidated, cartRequestController.deleteCartRequestById);
 
 module.exports = router;
 
